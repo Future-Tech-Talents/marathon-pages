09be2caf779ebc119e51f3b001fd8d0cec2f4e19
ivashev <andrew.ivashev@yandex.ru>
2022-07-22 01:57:56 +0400

    completes qa backend table

--- qa-test-backend/js/index.js
+++ qa-test-backend/js/index.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
+  const tableBody = $('#tableBody')
+
   $.get('https://backend.marathon.ivashev.com/find', function (data) {
     $("#container").removeClass('loading')
 
-
+    data.forEach(function(item) {
+      tableBody.append(
+        `
+          <tr>
+            <td>${item.name}</td>
+            <td>${item.age || ''}</td>
+            <td>${item.email}</td>
+          </tr>
+        `
+      )
+    })
   })
 })
